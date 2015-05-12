ce4f81c2ff30737612ae35067786a290f0c3f506
sebandia <sebastian.andia@inacpamail.cl>
2015-05-12 18:01:54 -0300

    Update agregar.php
    cambio

--- application/views/agregar.php
+++ application/views/agregar.php
@@ -6,7 +6,7 @@
 		<div class="row">
 			<div class="col-md-8">  
 			<center>
-	    	<h3>Agregar clientespizarrrrrrrrrrooooooo</h3> </center>  
+	    	<h3>Agregar clientesriffoooooo</h3> </center>  
 				<form class="form-horizontal" role="form" id="form" name="form" action="<?=base_url()?>index.php/main/guardar" method="POST">
 					<div class="form-group">
 			    	<label for="inputEmail3" class="col-sm-2 control-label">Nombre</label>
